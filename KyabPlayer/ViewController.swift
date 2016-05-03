0ea9e19227845035d875f137f3f58ccb28e40a69
kyab <kyossi212@gmail.com>
2016-05-04 02:54:02 +0900

    Changed to use HTML5MediaElement methods to get /set property

--- KyabPlayer/ViewController.swift
+++ KyabPlayer/ViewController.swift
@@ -19,11 +19,9 @@ class ViewController: NSViewController {
     @IBOutlet weak var sliderPosition: NSSlider!
     
     var youtubeURL : String?
-    var currentSec : Int = 0
-    var currentSecLastReliable : Int = 0
-    var targetSec : Int = 0
-    var totalSec : Int = 0
-    var date : NSDate!
+    var currentSec : Double = 0
+    var totalSec : Double = 0
+    var targetSec : Double = 0
     
     let VIDEO_ELEMENT = "document.getElementsByClassName('html5-main-video')[0]"
 
@@ -73,7 +71,7 @@ class ViewController: NSViewController {
     
     
     func webView(sender: WebView!, didFinishLoadForFrame frame: WebFrame!) {
-//        NSLog("delegate calld(didFinishLoadForFrame\n");
+        
     }
     
     func onTimer(t:NSTimer){
@@ -83,69 +81,50 @@ class ViewController: NSViewController {
             lblMovieTitle.stringValue = title
         }
         
-        var script = script_getFirstElementForClass("ytp-time-current")
-        let currentTime = webView.stringByEvaluatingJavaScriptFromString(script + ".innerHTML")
+        let currentTimeStr = webView.stringByEvaluatingJavaScriptFromString(VIDEO_ELEMENT + ".currentTime")
+        if (currentTimeStr != "") {
+            currentSec = Double(currentTimeStr)!
+            lblCurrentTime.stringValue =
+                String(format:"%.2d:%.2d", Int(currentSec) / 60, Int(currentSec) % 60 )
 
-        //Youtube上でコントロールが非表示な場合は信頼できない値
-        //コントロールが表示されている場合、ytp-chorme-controlsクラスである要素のcursorスタイルが"auto"
-        if currentTime != "" {
-            script = "document.defaultView.getComputedStyle(document.getElementsByClassName('ytp-chrome-controls')[0],'').cursor"
-            let cursorStyle = webView.stringByEvaluatingJavaScriptFromString(script)
-            if (cursorStyle == "auto") {
-                let timeComponents = currentTime.componentsSeparatedByString(":")
-                let prevCurrentSec = currentSec
-                currentSec = Int(timeComponents[0])! * 60 + Int(timeComponents[1])!
-                currentSecLastReliable = currentSec
-                if (prevCurrentSec != currentSec){
-                    date = NSDate()
-                }
-                
-                lblCurrentTime.stringValue =
-                    String(format:"%.2d:%.2d", currentSec / 60, currentSec % 60 )
-                //print("currentTime(HTML) = ", currentTime, ", seconds = ", currentSec)
-                
-            }else{
-                //print("currentTime is not reliable(", currentTime, ")")
-                currentSec = currentSecLastReliable + Int(NSDate().timeIntervalSinceDate(date))
-                lblCurrentTime.stringValue =
-                    String(format:"%.2d:%.2d", currentSec / 60, currentSec % 60 )
-            }
         }
         
-        
-        script = script_getFirstElementForClass("ytp-time-duration")
-        let duration = webView.stringByEvaluatingJavaScriptFromString(script + ".innerHTML")
-        if duration != "" {
-            lblTotalTime.stringValue = duration
+        let totalTimeStr = webView.stringByEvaluatingJavaScriptFromString(VIDEO_ELEMENT + ".duration")
+        if (totalTimeStr != "" && totalTimeStr != "NaN") {
+            totalSec = Double(totalTimeStr)!
+            lblTotalTime.stringValue =
+                String(format:"%.2d:%.2d", Int(totalSec) / 60, Int(totalSec) % 60 )
+        }else {
+            totalSec = 0.0
             
-            let timeComponents = duration.componentsSeparatedByString(":")
-            totalSec = Int(timeComponents[0])! * 60 + Int(timeComponents[1])!
-            
-            let currentPositionRatio = 1.0 * Double(currentSec) / Double(totalSec)
-            sliderPosition.doubleValue = currentPositionRatio
-//            print("currentSec = ", currentSec, ", totalSec = ", totalSec, "ratio = ", currentPositionRatio)
         }
+        
+        if (totalSec > 0.0) {
+            sliderPosition.doubleValue = currentSec / totalSec
+        }
+        
+
     }
     
     @IBAction func sliderChanged(sender: AnyObject) {
-        targetSec = Int(Double(totalSec) * sliderPosition.doubleValue)
+        targetSec = totalSec * sliderPosition.doubleValue
         lblCurrentTime.stringValue =
-            String(format:"%.2d:%.2d", targetSec / 60, targetSec % 60 )
+            String(format:"%.2d:%.2d", Int(targetSec) / 60, Int(targetSec) % 60 )
         
-        //Do the trick to detect change ensured.
-        //http://stackoverflow.com/questions/3919905/subclassing-nsslider-need-a-workaround-for-missing-mouse-up-events-cocoa-osx
-        //https://lists.apple.com/archives/cocoa-dev/2008/Oct/msg01251.html
-        NSObject.cancelPreviousPerformRequestsWithTarget(self, selector: #selector(ViewController.onSliderChangeDone), object: nil)
-        self.performSelector(#selector(ViewController.onSliderChangeDone), withObject: nil, afterDelay: 0.0)
+        webView.stringByEvaluatingJavaScriptFromString(VIDEO_ELEMENT + ".currentTime = " + targetSec.description)
+        
+//        //Do the trick to detect change ensured.
+//        //http://stackoverflow.com/questions/3919905/subclassing-nsslider-need-a-workaround-for-missing-mouse-up-events-cocoa-osx
+//        //https://lists.apple.com/archives/cocoa-dev/2008/Oct/msg01251.html
+//        NSObject.cancelPreviousPerformRequestsWithTarget(self, selector: #selector(ViewController.onSliderChangeDone), object: nil)
+//        self.performSelector(#selector(ViewController.onSliderChangeDone), withObject: nil, afterDelay: 0.0)
 
         
     }
     
     func onSliderChangeDone(){
         print("onSliderChangeDone")
-        let newURL = youtubeURL! + String(format:"&t=%dm%ds", targetSec / 60, targetSec % 60)
-        let req = NSURLRequest(URL: NSURL(string:newURL)!)
-        webView.mainFrame.loadRequest(req)
+        webView.stringByEvaluatingJavaScriptFromString(VIDEO_ELEMENT + ".currentTime = " + targetSec.description)
     }
     
     @IBAction func playOrPause(sender: AnyObject) {
